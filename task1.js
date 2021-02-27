cbfee6ae2694207380bdcf8ab13e1ce7b99d1944
chairul <akbarcha4@gmail.com>
2021-02-27 17:57:49 +0700

    initial project

---
+++ task1.js
@@ -0,0 +1,21 @@
+// maaf bang saya masukin inputnya pake prompt
+//saya ada beberapa codingan juga bang
+
+//regular fuction 1
+function nama(a){
+    document.write('halo ' + a + '<br/>');
+}
+nama(prompt('masukan nama'))
+
+// //regular fuction 2
+// function jumlah(a,b){
+//     let hasil = a * b
+//     return hasil
+// }
+// document.write(jumlah((prompt('nilai 1')),(prompt('nilai 2'))))
+
+// //arrow function
+// let pesan = (nama) => {
+//     document.write('<br/>' + 'halo ' + nama);
+// }
+// pesan(prompt('masukan nama'))
