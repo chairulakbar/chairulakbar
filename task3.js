a92a340a61e3d0574b596862e58e14ddf9783541
chairul <akbarcha4@gmail.com>
2021-02-27 18:18:28 +0700

    nambah task 3

---
+++ task3.js
@@ -0,0 +1,29 @@
+
+// maaf bang saya masukin inputnya pake prompt
+// di task 3 ada 3 soal bang (soal 2 sama 3 saya matiin bang biar promptnya nggak keluar semua bang)
+
+//soal 1 (naik 1 angka)
+function angka(a){
+   for(let nilaiInput = 1; nilaiInput <= a; nilaiInput++ ){
+        document.write(nilaiInput)
+    }
+}
+angka(prompt('masukan input \n nilai input :15'))
+
+
+// // soal ke 2 (kuadrat)
+// function kuadrat(a){
+//     for(let bilangan = 1; bilangan <= a; bilangan++ ){
+//         document.write(bilangan*bilangan)
+//     }
+// }
+// kuadrat(prompt('masukan input \n nilai input : 15'))
+
+
+// // soal ke 3 (kelipatan 3)
+// function kelipatan(a){
+//    for(let nilaiInput = 1; nilaiInput <= a; nilaiInput++ ){
+//         document.write(nilaiInput*3)
+//     }
+// }
+// kelipatan(prompt('masukan input \n input : 20'))
